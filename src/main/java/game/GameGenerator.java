51d34d497ec291fb5be3113f6d523ebf52347b7d
Cameron Long <7832325+cdalong@users.noreply.github.com>
2022-12-23 12:54:37 -0500

    Playing 200 games

--- src/main/java/game/GameGenerator.java
+++ src/main/java/game/GameGenerator.java
@@ -2,6 +2,8 @@ package game;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import model.GameMetadata;
 import player.Player;
 import player.PlayerType;
 
@@ -10,7 +12,13 @@ public class GameGenerator {
   private static final org.apache.log4j.Logger LOGGER =
       org.apache.log4j.Logger.getLogger(GameGenerator.class.getName());
 
+  private static final List<GameMetadata> listOfGamesPlayed = new ArrayList<>();
+
+
   public static void main(String[] args) {
+
+    for (int i = 0; i < 200; i ++)
+    {
 
     List<Player> gamePlayers = new ArrayList<>();
     Player cameron = new Player(PlayerType.PLAYER_TYPE.AGGRESSIVE, 500, 3);
@@ -19,5 +27,15 @@ public class GameGenerator {
     fisk.setName("Fisk");
     gamePlayers.add(cameron);
     gamePlayers.add(fisk);
+
+    Dice diceGame = new Dice(gamePlayers);
+    diceGame.playGame();
+
+    GameMetadata metadata = diceGame.metadata;
+      listOfGamesPlayed.add(metadata);
+
+    }
+    LOGGER.info("Games finished");
   }
+
 }
