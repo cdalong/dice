257d229b06807f23ae4885f91750c5b85fade634
Cameron Long <7832325+cdalong@users.noreply.github.com>
2022-12-23 11:46:38 -0500

    Metadata object

--- src/main/java/model/GameMetadata.java
+++ src/main/java/model/GameMetadata.java
@@ -3,12 +3,21 @@ package model;
 import lombok.Builder;
 import lombok.Data;
 import player.Player;
+import player.PlayerType;
+
+import java.util.List;
 
 @Data
 @Builder
 public class GameMetadata {
     int totalTurns;
+
+    List<Player> players;
+
     Player winningPlayer;
 
+    PlayerType winningPlayerType;
+
+    int winningPlayerAverageRollScore;
 
 }
